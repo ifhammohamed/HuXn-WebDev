a71af98c52bf661e35e69004cf827a0280611d4f
ifham-IM <ifhammohamed1111@gmail.com>
2024-10-30 21:33:57 +0530

    main and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Add from "./components/Add";
+import Footer from "./components/Footer";
 import Greet from "./components/Greet";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 const App = () => {
   return <div>
     <Header />
+
+    <Main />
+
+    <Footer />
   </div>
 }
 
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <div>
+            <h2>Main Content</h2>
+
+            <p>This is main content.</p>
+        </div>
+    )
+}
+
+export default Main
